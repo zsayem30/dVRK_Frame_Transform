d9c027540b15a8c0370d46aecebc425637b7b2e9
zsayem30 <sayemnazmuz@gmail.com>
2021-08-24 19:30:47 -0700

    Added transformation for ECM camera. There appears to be some deviation of the PSM tool tip while teleoperating. Need to look into this more.

--- psm_frame_transform.py
+++ psm_frame_transform.py
@@ -25,17 +25,24 @@ def update_transforms(): #update the transforms
 	
 	tf_psm1_to_cam = tf_to_pykdl_frame(tf_listener.lookupTransform('Vision_sensor_left', 'J1_PSM1',rospy.Time()))
 	tf_psm2_to_cam = tf_to_pykdl_frame(tf_listener.lookupTransform('Vision_sensor_left', 'J1_PSM2',rospy.Time()))
+	tf_ecm_to_cam = tf_to_pykdl_frame(tf_listener.lookupTransform('Vision_sensor_left', 'J1_ECM',rospy.Time()))
 
 	PSM_J1_TO_BASE_LINK = PyKDL.Frame(PyKDL.Rotation.RPY(math.pi/2.0, -math.pi, 0), PyKDL.Vector())
+	ECM_J1_TO_BASE_LINK = PyKDL.Frame(PyKDL.Rotation.RPY(math.pi/2.0, -math.pi/2.0, 0), PyKDL.Vector())
+
 	fix = PyKDL.Frame(PyKDL.Rotation.RPY(0,math.pi,0), PyKDL.Vector())
 
 	frame1 =  fix * tf_psm1_to_cam * PSM_J1_TO_BASE_LINK
 	frame2 = fix * tf_psm2_to_cam * PSM_J1_TO_BASE_LINK
+	frame3 = fix * tf_ecm_to_cam * ECM_J1_TO_BASE_LINK
 
 	rot1 = frame1.M.GetQuaternion()
 	rot2 = frame2.M.GetQuaternion()
+	rot3 = frame3.M.GetQuaternion()
+
 	trans1 = frame1.p
 	trans2 = frame2.p
+	trans3 = frame3.p
 
 	pose1.position.x = trans1[0]
 	pose1.position.y = trans1[1]
@@ -55,17 +62,26 @@ def update_transforms(): #update the transforms
 	pose2.orientation.z = rot2[2]
 	pose2.orientation.w = rot2[3]
 
+	pose3.position.x = trans3[0]
+	pose3.position.y = trans3[1]
+	pose3.position.z = trans3[2]
+	
+	pose3.orientation.x = rot3[0]
+	pose3.orientation.y = rot3[1]
+	pose3.orientation.z = rot3[2]
+	pose3.orientation.w = rot3[3]
+
 pub1 = rospy.Publisher('/dvrk/PSM1/set_base_frame', Pose, queue_size=10)
 pub2 = rospy.Publisher('/dvrk/PSM2/set_base_frame', Pose, queue_size=10)
-
+pub3 = rospy.Publisher('/dvrk/ECM/set_base_frame', Pose, queue_size=10)
 
 rate = rospy.Rate(100) # 10hz
 pose1 = Pose()
 pose2 = Pose()
+pose3 = Pose()
 while not rospy.is_shutdown():
 	update_transforms()
 	pub1.publish(pose1)
 	pub2.publish(pose2)
+	pub3.publish(pose3)
 	rate.sleep()
-	print(pose1)
-	print(pose2)
